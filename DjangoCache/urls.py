73c585701c28f41f8282499539b3de15a159aef9
Rohit Thakre <thake785@gmail.com>
2024-05-15 22:38:34 +0530

    model and mingrations added.

--- DjangoCache/urls.py
+++ DjangoCache/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls', namespace="base")),
+    path('', include('base.urls'),name="base"),
 ]
--- base/models.py
+++ base/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Student(models.Model):
+    student = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15)
+    address = models.TextField(null=True, blank=True)
+    eduction = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.phone
