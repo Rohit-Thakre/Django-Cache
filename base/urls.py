61b5db00133fa5b13c69e9f0d2b50dc683da6cc4
Rohit Thakre <thake785@gmail.com>
2024-05-16 22:55:32 +0530

    home view added..

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from base import views
 urlpatterns = [
-#  path("", views.Home.as_view(), name="home")
+ path("", views.Home.as_view(), name="home")
 ]
--- base/views.py
+++ base/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from base import models
+from rest_framework import status
 
-# Create your views here.
+class Home(APIView):
+    permission_classes =  [ IsAuthenticated]
+
+    def initial(self, request, *args, **kwargs):
+        return super().initial(request, *args, **kwargs)
+    
+
+    def get(self,request,*args, **kwargs): 
+        students = models.Student.objects.all().values()
+        return Response(data=students, status=status.HTTP_200_OK)
+    
+    def post(self,request,*args,**kwargs):
+        return Response(data={}, status=status.HTTP_201_CREATED)
+    
+    def put(self,request,*args,**kwargs):
+        return Response(data={}, status=status.HTTP_201_CREATED)
+
+    def patch(self,request,  *args,**kwargs):
+        return Response(data={}, status=status.HTTP_200_OK)
